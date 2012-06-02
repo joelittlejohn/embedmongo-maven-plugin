ff4bdcc9e38c2e4e0569553fa60a6c024f587ce7
Joe Littlejohn <joelittlejohn@gmail.com>
2012-06-02 20:18:09 +0100

    Improve start goal docs

--- src/main/java/com/github/joelittlejohn/embedmongo/StartEmbeddedMongoMojo.java
+++ src/main/java/com/github/joelittlejohn/embedmongo/StartEmbeddedMongoMojo.java
@@ -29,6 +29,7 @@ import de.flapdoodle.embedmongo.MongodProcess;
 import de.flapdoodle.embedmongo.config.MongodConfig;
 import de.flapdoodle.embedmongo.distribution.Version;
 import de.flapdoodle.embedmongo.runtime.Network;
+import static java.util.Arrays.*;
 
 /**
  * When invoked, this goal starts an instance of mongo. The required binaries
@@ -41,84 +42,92 @@ import de.flapdoodle.embedmongo.runtime.Network;
  */
 public class StartEmbeddedMongoMojo extends AbstractMojo {
 
-	public static final String MONGOD_CONTEXT_PROPERTY_NAME = StartEmbeddedMongoMojo.class.getPackage().getName() + ".mongod";
+    private static final String PACKAGE_NAME = StartEmbeddedMongoMojo.class.getPackage().getName();
+    public static final String MONGOD_CONTEXT_PROPERTY_NAME = PACKAGE_NAME + ".mongod";
 
-	/**
-     * @parameter expression="${embedmongo.port}"
-     *            default-value="27017"
+    /**
+     * The port MongoDB should run on.
+     * 
+     * @parameter expression="${embedmongo.port}" default-value="27017"
      * @since 0.1.0
-	 */
-	private int port;
-	
-	/**
-     * @parameter expression="${embedmongo.version}"
-     *            default-value="V2_1_1"
-	 */
-	private String version;
-	
-	/**
+     */
+    private int port;
+
+    /**
+     * The version of MongoDB to run e.g. 2.1.1, 1.6 v1.8.2, V2_0_4,
+     * 
+     * @parameter expression="${embedmongo.version}" default-value="V2_1_1"
+     * @since 0.1.0
+     */
+    private String version;
+
+    /**
+     * The location of a directory that will hold the MongoDB data files.
+     * 
      * @parameter expression="${embedmongo.databaseDirectory}"
-	 */
-	private File databaseDirectory;
-	
+     * @since 0.1.0
+     */
+    private File databaseDirectory;
+
     @Override
     @SuppressWarnings("unchecked")
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-		MongodExecutable executable;
-		try {
-			 executable = MongoDBRuntime.getDefaultInstance().prepare(new MongodConfig(getVersion(), port, Network.localhostIsIPv6(), getDataDirectory()));
-		} catch (UnknownHostException e) {
-			throw new MojoExecutionException("Unable to determine if localhost is ipv6", e);
-		}
-		
-		try {
-			MongodProcess mongod = executable.start();
-		    addShutdownHook(mongod);
-		    
-		    getPluginContext().put(MONGOD_CONTEXT_PROPERTY_NAME, mongod);
+        MongodExecutable executable;
+        try {
+            executable = MongoDBRuntime.getDefaultInstance().prepare(new MongodConfig(getVersion(), port, Network.localhostIsIPv6(), getDataDirectory()));
+        } catch (UnknownHostException e) {
+            throw new MojoExecutionException("Unable to determine if localhost is ipv6", e);
+        }
+
+        try {
+            MongodProcess mongod = executable.start();
+            addShutdownHook(mongod);
+
+            getPluginContext().put(MONGOD_CONTEXT_PROPERTY_NAME, mongod);
         } catch (IOException e) {
-	        throw new MojoExecutionException("Unable to start the mongod", e);
+            throw new MojoExecutionException("Unable to start the mongod", e);
         }
     }
 
     private Version getVersion() throws MojoExecutionException {
-        String flapdoodleCompatibleVersionString = this.version.replaceAll("\\.", "_"); 
-        
+        String flapdoodleCompatibleVersionString = this.version.toUpperCase().replaceAll("\\.", "_");
+
         if (this.version.charAt(0) != 'V') {
             flapdoodleCompatibleVersionString = "V" + flapdoodleCompatibleVersionString;
         }
-        
+
         try {
             return Version.valueOf(flapdoodleCompatibleVersionString);
         } catch (IllegalArgumentException e) {
-            throw new MojoExecutionException("Unrecognised mongo version: " + this.version, e);
+            throw new MojoExecutionException("Unrecognised MongoDB version: '" + this.version + 
+                    "', try one of the following: \n" + asList(Version.class.getEnumConstants()) + "\n", e);
         }
-        
-    }
-    
-	private String getDataDirectory() {
-		if (databaseDirectory != null) {
-			return databaseDirectory.getAbsolutePath();
-		} else {
-			return null;
-		}
+
     }
 
-	/**
-	 * A final failsafe to shutdown the mongo instance at the end of the build,
-	 * even if the plugin stop goal is not invoked.
-	 * 
-	 * @param mongod
-	 *            the mongo process started by this plugin
-	 */
-	private void addShutdownHook(final MongodProcess mongod) {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-		    	mongod.stop();
-			}
-		});
-	}
-	
+    private String getDataDirectory() {
+        if (databaseDirectory != null) {
+            return databaseDirectory.getAbsolutePath();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * A final failsafe to shutdown the mongod process at the end of the build,
+     * even if the plugin stop goal is not invoked.
+     * 
+     * @param mongod
+     *            the mongo process started by this plugin
+     */
+    private void addShutdownHook(final MongodProcess mongod) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                mongod.stop();
+            }
+        });
+    }
+
 }
